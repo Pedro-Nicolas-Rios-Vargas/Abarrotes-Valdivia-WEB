7ea033c573e1540181209bed02c96587219b9475
Pedro-Nicolas-Rios-Vargas <pv123124@gmail.com>
2021-08-31 21:38:57 -0500

    Se ajustaron algunos problemas de seguridad.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ol@v48hk#_#1y=hmv^f-3um3$8ujnt*i%dcdrix0f%mdo@2_z('
+with open('./secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
