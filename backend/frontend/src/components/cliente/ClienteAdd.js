81ba7f4d6ae8463c3c6b7bd2d172344490e5e81f
VRVG1 <viravago@gmail.com>
2021-10-12 14:38:46 -0500

    Ya funciona el login, pero aun  no valida usuarios, ya se agrego las validaciones pertinentes de cliente agregar y ya busca clientes en consultar

--- backend/frontend/src/components/cliente/ClienteAdd.js
+++ backend/frontend/src/components/cliente/ClienteAdd.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 
 export default class ClienteAdd extends Component {
-    defaultName = "hola"
-    defualtBalance = -5.5
+    defaultName = ""
+    defualtBalance = ""
     constructor(props) {
         super(props);
         this.state = {
@@ -23,45 +23,61 @@ export default class ClienteAdd extends Component {
     }
 
     getbalance(e) {
-        this.setState({
-            balance: e.target.value,
-        });
+        if (/^([-]?\d*)([.]\d{0,2})?$/.test(e.target.value)) {
+            this.setState({
+                balance: e.target.value,
+            });
+        } else {
+            this.setState({
+                balance: this.state.balance,
+            })
+        }
+        if(this.state.balance.length === 1) {
+        }
     }
 
     AddClient() {
         //console.log(this)
-        console.log(this.state.nombre_C)
-        const requiestClient = {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                nombre_C: this.state.nombre_C,
-                balance: this.state.balance,
-            }),
-        };
-        fetch("/cliente/crear-cliente", requiestClient)
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+        if(this.state.nombre_C === "" || this.state.balance === ""){
+            alert("No se puede agregar un cliente sin nombre o sin saldo")
+        } else {
+            console.log(this.state.nombre_C)
+            const requiestClient = {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    nombre_C: this.state.nombre_C,
+                    balance: this.state.balance,
+                }),
+            };
+            fetch("/cliente/crear-cliente", requiestClient)
+                .then((response) => response.json())
+                .then((data) => console.log(data));
+            alert("Se agrego el cliente")
+            this.setState({ 
+                nombre_C:"",
+                balance:"",
+            })
+        }
     }
 
     render() {
         return (
             <div className="container">
-                <h1>Pene</h1>
                 <h2>Agregar Cliente
                     <small>Datos del cliente</small>
                 </h2>
 
                 <form>
                     <div className="group">
-                        <input type="text" required onChange={e => this.getNameClient(e)} />
+                        <input type="text" name="nombre_C" value={this.state.nombre_C} required onChange={e => this.getNameClient(e)} />
                         <span className="highlight"></span>
                         <span className="bar"></span>
                         <label>Nombre</label>
                     </div>
 
                     <div className="group">
-                        <input type="text" required onChange={e => this.getbalance(e)} />
+                        <input type="text" name="balance" value={this.state.balance} required onChange={e => this.getbalance(e)} />
                         <span className="highlight"></span>
                         <span className="bar"></span>
                         <label>Saldo</label>
@@ -69,8 +85,7 @@ export default class ClienteAdd extends Component {
 
                 </form>
                 <div className="footer">
-                    <button className="btn" onClick={() => this.AddClient()} >Agregar</button>
-                    <button className="btn back">Regresar</button>
+                    <button className="btn" onClick={() => this.AddClient()}>Agregar</button>
                 </div>
             </div>
         );
